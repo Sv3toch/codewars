5b95256581c4d45163ae7b95396725b379732acd
Sv3toch <svititchs@yandex.by>
2024-09-05 15:07:57 +0300

    Two-to-One.js

---
+++ 8kyu/Two-to-One.js
@@ -0,0 +1,27 @@
+/*
+Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string (alphabetical ascending), the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+
+Examples:
+a = "xyaabbbccccdefww"
+b = "xxxxyyyyabklmopq"
+longest(a, b) -> "abcdefklmopqwxy"
+
+a = "abcdefghijklmnopqrstuvwxyz"
+longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+ */
+
+//Submit
+
+function longest(s1, s2) {
+    let arr=[]
+    let arr2 =(s1+s2).split('')
+    for(i of (arr2) ){
+        if (arr.includes(i) == false){
+            arr.push(i)
+        }
+    }
+
+    return arr.sort().join('')
+}
+
+console.log(longest('asads', 'dasdadmkjn'))
