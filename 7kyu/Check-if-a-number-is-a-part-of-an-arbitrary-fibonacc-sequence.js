916623c9f05c27793f14a2c30e3da9b49ea534db
Sv3toch <svititchs@yandex.by>
2024-09-09 10:21:45 +0300

    Check-if-a-number-is-a-part-of-an-arbitrary-fibonacc-sequence.js

---
+++ 7kyu/Check-if-a-number-is-a-part-of-an-arbitrary-fibonacc-sequence.js
@@ -0,0 +1,29 @@
+/*
+You will be given a function, isFibo, which will take three arguments: a, b and f.
+
+a is the first term of a Fibonacci sequence, b is the second term of this sequence ( it will always be bigger than or equal to a and they'll be both always positive, so don't worry about pesky input checking ) and f is a number which you'll need to check whether or not is a part of this Fibonacci sequence.
+
+Good luck, muh fellow coding warrior.
+ */
+
+
+//submit
+
+function isFibo(a, b, f) {
+    if (f === a || f === b) {
+        return true
+    }
+
+    for (a; b < f; b) {
+
+        a = a + b;
+        b = a + b;
+        if (f === a || f === b) {
+            return true
+        }
+
+    }
+    return false
+}
+
+console.log(isFibo(0, 1, 2))
