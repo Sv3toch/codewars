ef77e9d1e4b4abe2d62088e33a86841930b5a01e
Sv3toch <svititchs@yandex.by>
2025-08-20 22:20:16 +0300

    Reverse-words.js

---
+++ 7kyu/Reverse-words.js
@@ -0,0 +1,15 @@
+// Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+//
+//     Examples
+// "This is an example!" ==> "sihT si na !elpmaxe"
+// "double  spaces"      ==> "elbuod  secaps"
+
+function reverseWords(str) {
+    return str.split(' ').map(n=>[...n].reverse().join('')).join(' ')
+}
+
+
+console.log(reverseWords('apple'))
+console.log(reverseWords('The quick brown fox jumps over the lazy dog.'))
+console.log(reverseWords('a b c d'))
+console.log(reverseWords('  double  spaced  words  '))
