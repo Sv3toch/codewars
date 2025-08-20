69a76d6cf01504ce52f7d120f34c53b1f44619fb
Sv3toch <svititchs@yandex.by>
2025-08-20 23:18:00 +0300

    MakeUpperCase.js

---
+++ 8kyu/MakeUpperCase.js
@@ -0,0 +1,11 @@
+// Write a function which converts the input string to uppercase.
+
+
+function makeUpperCase(str) {
+   return str.toUpperCase()
+}
+
+
+console.log(makeUpperCase("hello"))
+console.log(makeUpperCase("Hello"))
+console.log(makeUpperCase("HELLO"))
