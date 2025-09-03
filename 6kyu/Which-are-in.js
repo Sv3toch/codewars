063d8e718eaaaab6d5e1cdd53b02726b479ed1ae
Sv3toch <svititchs@yandex.by>
2025-09-03 15:14:00 +0300

    Which-are-in.js

---
+++ 6kyu/Which-are-in.js
@@ -0,0 +1,45 @@
+// Given two arrays of strings a1 and a2 return a sorted array r in lexicographical order of the strings of a1 which are substrings of strings of a2.
+//
+//     Example 1:
+// a1 = ["arp", "live", "strong"]
+//
+// a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
+//
+// returns ["arp", "live", "strong"]
+//
+// Example 2:
+// a1 = ["tarp", "mice", "bull"]
+//
+// a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
+//
+// returns []
+//
+// Notes:
+//     Arrays are written in "general" notation. See "Your Test Cases" for examples in your language.
+//     In Shell bash a1 and a2 are strings. The return is a string where words are separated by commas.
+//     Beware: In some languages r must be without duplicates.
+
+
+function inArray(array1,array2){
+    let newArr= array1.reduce((acc,el)=>{
+        array2.forEach(n=>{ if (n.includes(el)){
+            console.log(1)
+            acc= [...acc,el]
+        }
+        })
+           return acc
+
+    },[])
+    newArr =[ ...(new Set(newArr))].sort()
+
+    return newArr
+}
+
+
+const a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
+a1 = ["xyz", "live", "strong"]
+console.log(inArray(a1, a2))
+a1 = ["live", "strong", "arp"]
+console.log(inArray(a1, a2))
+a1 = ["tarp", "mice", "bull"]
+console.log(inArray(a1, a2))
