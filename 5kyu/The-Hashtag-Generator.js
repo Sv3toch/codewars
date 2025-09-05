f31de1f2129298880c589ed7e1305f75606e4b9d
Sv3toch <svititchs@yandex.by>
2025-09-05 12:59:49 +0300

    The-Hashtag-Generator.js

---
+++ 5kyu/The-Hashtag-Generator.js
@@ -0,0 +1,29 @@
+// The marketing team is spending way too much time typing in hashtags.
+//     Let's help them with our own Hashtag Generator!
+//
+// Here's the deal:
+//
+// It must start with a hashtag (#).
+// All words must have their first letter capitalized.
+//     If the final result is longer than 140 chars it must return false.
+//     If the input or the result is an empty string it must return false.
+//     Examples
+// " Hello there thanks for trying my Kata"  =>  "#HelloThereThanksForTryingMyKata"
+// "    Hello     World   "                  =>  "#HelloWorld"
+// ""                                        =>  false
+
+
+
+function generateHashtag (str) {
+    const arr= str.split(' ')
+const result = arr.reduce((acc,el)=>`${acc}${el.slice(0,1).toUpperCase()+el.slice(1)}`,'#')
+    if(result.length>140 || result.length===1)return false
+
+    return result
+}
+
+
+console.log(generateHashtag(''))
+console.log(generateHashtag("Do We have A Hashtag"))
+console.log(generateHashtag("Codewars is nice"))
+console.log(generateHashtag('a'.repeat(140)))
