80aace40c368ae0bdcd22f8168e88ced8d56abcb
Sv3toch <svititchs@yandex.by>
2025-09-30 23:57:17 +0300

    Round-up-to-the-next-multiple-of-5.js

---
+++ 7kyu/Round-up-to-the-next-multiple-of-5.js
@@ -0,0 +1,34 @@
+// Given an integer as input, can you round it to the next (meaning, "greater than or equal") multiple of 5?
+//
+//     Examples:
+//
+//     input:    output:
+//     0    ->   0
+// 2    ->   5
+// 3    ->   5
+// 12   ->   15
+// 21   ->   25
+// 30   ->   30
+// -2   ->   0
+// -5   ->   -5
+// etc.
+//     Input may be any positive or negative integer (including 0).
+//
+// You can assume that all inputs are valid integers.
+
+
+
+
+function roundToNext5(n){
+    return n<=0?n-(n%5):n%5===0?n:n+5-n%5
+}
+
+
+console.log(roundToNext5(0))
+console.log(roundToNext5(1))
+console.log(roundToNext5(5))
+console.log(roundToNext5(6))
+console.log(roundToNext5(-1))
+console.log(roundToNext5(-8))
+console.log(roundToNext5(-11))
+console.log(roundToNext5(-5))
