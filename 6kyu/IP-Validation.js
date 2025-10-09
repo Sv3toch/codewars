f0d17be7a4832d135f9a3b7b45f439a0e91bdacb
Sv3toch <svititchs@yandex.by>
2025-10-09 14:36:29 +0300

    IP-Validation.js

---
+++ 6kyu/IP-Validation.js
@@ -0,0 +1,32 @@
+// Write an algorithm that will identify valid IPv4 addresses in dot-decimal format. IPs should be considered valid if they consist of four octets, with values between 0 and 255, inclusive.
+//
+//     Valid inputs examples:
+//     Examples of valid inputs:
+//     1.2.3.4
+// 123.45.67.89
+// Invalid input examples:
+//     1.2.3
+// 1.2.3.4.5
+// 123.456.78.90
+// 123.045.067.089
+// Notes:
+//     Leading zeros (e.g. 01.02.03.04) are considered invalid
+// Inputs are guaranteed to be a single string
+
+function isValidIP(str) {
+    const arr=str.split('.')
+    return  arr.length===4 && arr.every((el)=>el.length===String(Number(el)).length&&Number(el)<256&&Number(el)>=0)
+}
+
+
+console.log(isValidIP("0.0.0.0"  ))
+console.log(isValidIP("12.255.56.1"))
+console.log(isValidIP("12.255.5e6.1"))
+console.log(isValidIP("137.255.156.100"))
+console.log(isValidIP('abc.def.ghi.jkl'))
+console.log(isValidIP('12.34.56.-7'))
+console.log(isValidIP(''))
+console.log(isValidIP('1.2.3.4\n' ))
+console.log(isValidIP('\n1.2.3.4'))
+
+
