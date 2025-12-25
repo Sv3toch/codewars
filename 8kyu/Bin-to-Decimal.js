4a7c500bf4a4c62abda200810d5e888f1575f72d
Sv3toch <svititchs@yandex.by>
2025-12-25 21:31:53 +0300

    Bin-to-Decimal.js

---
+++ 8kyu/Bin-to-Decimal.js
@@ -0,0 +1,14 @@
+// Complete the function which converts a binary number (given as a string) to a decimal number.
+//
+
+
+function binToDec(bin) {
+   return parseInt(bin,2)
+}
+
+
+console.log(binToDec('0'))
+console.log(binToDec('10'))
+console.log(binToDec('11100'))
+console.log(binToDec('1001001'))
+console.log(binToDec('1111111111'))
